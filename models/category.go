c6dfc5c23255717254eeaf195e9cd8b727ee3657
Tegar Aditia Pratama <tegara946@gmail.com>
2021-09-09 13:41:59 +0700

    initial models product and category

---
+++ models/category.go
@@ -0,0 +1,8 @@
+package models
+
+import "gorm.io/gorm"
+
+type Categories struct {
+	gorm.Model
+	Name string `json:"category"`
+}
--- models/product.go
+++ models/product.go
@@ -1,1 +1,12 @@
 package models
+
+import "gorm.io/gorm"
+
+type ProductsSchema struct {
+	gorm.Model
+	Category    Categories
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       int    `json:"price"`
+	Stock       int    `json:"stock"`
+}
