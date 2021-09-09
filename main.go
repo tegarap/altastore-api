7a895838921c49cb707aa28dd3a794d24b6acb5c
urnikrokhiyah <urnikrokhiyah123@gmail.com>
2021-09-09 19:40:40 +0700

    add log middleware feature

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/tegarap/altastore-api/config"
+	"github.com/tegarap/altastore-api/lib/middleware"
 	"github.com/tegarap/altastore-api/routes"
 )
 
 func main() {
 	config.InitDb()
 	e := routes.New()
+	middleware.LogMiddlewares(e)
 	e.Logger.Fatal(e.Start(":8000"))
 }
