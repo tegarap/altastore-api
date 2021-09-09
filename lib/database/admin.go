c19c7aaa4cd5e503b8377e8e76190ac8edbd8ca1
Tegar Aditia Pratama <tegara946@gmail.com>
2021-09-09 14:21:00 +0700

    fixing var DB to Db

--- lib/database/admin.go
+++ lib/database/admin.go
@@ -1,17 +1,20 @@
 package database
 
-import "github.com/tegarap/altastore-api/models"
+import (
+	"github.com/tegarap/altastore-api/config"
+	"github.com/tegarap/altastore-api/models"
+)
 
 func LoginAdmin(admin *models.Admins) (interface{}, error){
 	var err error
-	if err = config.DB.Where("email = ? AND password = ?", admin.Email, admin.Password).First(admin).Error; err != nil {
+	if err = config.Db.Where("email = ? AND password = ?", admin.Email, admin.Password).First(admin).Error; err != nil {
 		return nil, err
 	}
 	return admin, nil
 }
 
 func RegisterAdmin(admin *models.Admins) (interface{}, error) {
-	if err := config.DB.Create(&admin).Error; err != nil {
+	if err := config.Db.Create(&admin).Error; err != nil {
 		return nil, err
 	}
 	return admin, nil
--- lib/database/customer.go
+++ lib/database/customer.go
@@ -1,16 +1,19 @@
 package database
 
-import "github.com/tegarap/altastore-api/models"
+import (
+	"github.com/tegarap/altastore-api/config"
+	"github.com/tegarap/altastore-api/models"
+)
 
 func LoginCustomer(customer *models.Customers) (interface{}, error){
-	if err := config.DB.Where("email = ? AND password = ?", customer.Email, customer.Password).First(customer).Error; err != nil {
+	if err := config.Db.Where("email = ? AND password = ?", customer.Email, customer.Password).First(customer).Error; err != nil {
 		return nil, err
 	}
 	return customer, nil
 }
 
 func RegisterCustomer(customer *models.Customers) (interface{}, error) {
-	if err := config.DB.Create(&customer).Error; err != nil {
+	if err := config.Db.Create(&customer).Error; err != nil {
 		return nil, err
 	}
 	return customer, nil
