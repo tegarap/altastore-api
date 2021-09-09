8e0994b6a0772b13ddf3bec83c40ae534619750d
urnikrokhiyah <urnikrokhiyah123@gmail.com>
2021-09-09 12:34:04 +0700

    add configuration to connect databases

--- config/config.go
+++ config/config.go
@@ -1,1 +1,26 @@
 package config
+
+import (
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var Db *gorm.DB
+
+func initDb() {
+	connection := os.Getenv("CONNECTION")
+
+	var err error
+	Db, err = gorm.Open(mysql.Open(connection), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	InitMigrate()
+}
+
+func InitMigrate() {
+	Db.AutoMigrate()
+	Db.AutoMigrate()
+}
