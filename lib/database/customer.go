2b70fd1c8b69a31cd5e30a622f57f2b1914d9b71
Tegar Aditia Pratama <tegara946@gmail.com>
2021-09-09 13:36:46 +0700

    feature register & login customer

--- lib/database/customer.go
+++ lib/database/customer.go
@@ -1,1 +1,17 @@
 package database
+
+import "github.com/tegarap/altastore-api/models"
+
+func LoginCustomer(customer *models.Customers) (interface{}, error){
+	if err := config.DB.Where("email = ? AND password = ?", customer.Email, customer.Password).First(customer).Error; err != nil {
+		return nil, err
+	}
+	return customer, nil
+}
+
+func RegisterCustomer(customer *models.Customers) (interface{}, error) {
+	if err := config.DB.Create(&customer).Error; err != nil {
+		return nil, err
+	}
+	return customer, nil
+}
