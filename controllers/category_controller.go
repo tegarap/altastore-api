a9fa538088b22bf5febbc45641bb1eb3eeed278e
urnikrokhiyah <urnikrokhiyah123@gmail.com>
2021-09-10 08:08:43 +0700

    add get single category controller

--- controllers/category_controller.go
+++ controllers/category_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/tegarap/altastore-api/lib/database"
@@ -18,4 +19,16 @@ func CreateNewCategoriesController(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, util.ResponseFail("failed", nil))
 	}
 	return c.JSON(http.StatusOK, util.ResponseSuccess("success", newCategory))
+}
+
+func GetSingleCategoryController(c echo.Context) error {
+	categoryId, errorId := strconv.Atoi(c.Param("id"))
+	if errorId != nil {
+		return c.JSON(http.StatusBadRequest, util.ResponseFail("failed", nil))
+	}
+	category, err := database.GetSingleCategory(categoryId)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, util.ResponseFail("failed", nil))
+	}
+	return c.JSON(http.StatusOK, util.ResponseSuccess("success", category))
 }
