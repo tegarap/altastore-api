90e9e8289c2d4acf6f36d758662fa1357ed27a46
Tegar Aditia Pratama <tegara946@gmail.com>
2021-09-12 01:26:12 +0700

    add jwt middleware file on lib/middleware

--- lib/middleware/jwt_middleware.go
+++ lib/middleware/jwt_middleware.go
@@ -1,1 +1,27 @@
 package middleware
+
+import (
+	"github.com/golang-jwt/jwt"
+	"github.com/labstack/echo/v4"
+	"os"
+	"time"
+)
+
+func CreateToken(userId int) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["userId"] = userId
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(os.Getenv("SECRET_JWT")))
+}
+
+func ExtractTokenUserId(e echo.Context) int {
+	user := e.Get("user").(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		userId := claims["userId"].(float64)
+		return int(userId)
+	}
+	return 0
+}
