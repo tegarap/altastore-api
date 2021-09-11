e89c154d0e7d65fd48314b387a9b2ada3701a59e
Tegar Aditia Pratama <tegara946@gmail.com>
2021-09-12 00:59:12 +0700

    fixing json response admin

--- lib/database/admin.go
+++ lib/database/admin.go
@@ -10,14 +10,12 @@ func LoginAdmin(admin *models.Admins) (interface{}, error){
 	if err = config.Db.Where("email = ? AND password = ?", admin.Email, admin.Password).First(admin).Error; err != nil {
 		return nil, err
 	}
-	outAdmin := models.AdminResponse{}
-	return outAdmin, nil
+	return admin, nil
 }
 
 func RegisterAdmin(admin *models.Admins) (interface{}, error) {
 	if err := config.Db.Create(&admin).Error; err != nil {
 		return nil, err
 	}
-	outAdmin := models.AdminResponse{}
-	return outAdmin, nil
+	return admin, nil
 }
--- models/customer.go
+++ models/customer.go
@@ -6,7 +6,7 @@ type Customers struct {
 	gorm.Model
 	Name         string `json:"name" form:"name"`
 	Email        string `json:"email" form:"email"`
-	Password     string `json:"-" form:"password"`
+	Password     string `json:"password" form:"password"`
 	Phone        int    `json:"phone" form:"phone"`
 	Address      string `json:"address" form:"address"`
 	Gender       string `json:"gender" sql:"type:ENUM('male', 'female')"`
