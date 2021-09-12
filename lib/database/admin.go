c0ee831733ba95f34e9be56f8429d49a5f90d6d0
Tegar Aditia Pratama <tegara946@gmail.com>
2021-09-12 10:55:56 +0700

    add jwt create token on login admin

--- lib/database/admin.go
+++ lib/database/admin.go
@@ -2,12 +2,20 @@ package database
 
 import (
 	"github.com/tegarap/altastore-api/config"
+	"github.com/tegarap/altastore-api/lib/middleware"
 	"github.com/tegarap/altastore-api/models"
 )
 
 func LoginAdmin(admin *models.Admins) (interface{}, error){
 	var err error
 	if err = config.Db.Where("email = ? AND password = ?", admin.Email, admin.Password).First(admin).Error; err != nil {
+		return nil, err
+	}
+	admin.Token, err = middleware.CreateToken(int(admin.ID), true)
+	if err != nil {
+		return nil, err
+	}
+	if err = config.Db.Save(admin).Error; err != nil {
 		return nil, err
 	}
 	return admin, nil
--- models/admin.go
+++ models/admin.go
@@ -9,6 +9,7 @@ type Admins struct {
 	Name      string     `json:"name"`
 	Email     string     `json:"email"`
 	Password  string     `json:"password"`
+	Token     string     `json:"token"`
 	CreatedAt time.Time  `gorm:"column:created_at" json:"-"`
 	UpdatedAt time.Time  `gorm:"column:updated_at" json:"-"`
 	DeletedAt *time.Time `gorm:"column:deleted_at" json:"-"`
